6c49279943b3e7a293a1ace76edb01b71e157fe2
Daniel D GONCALVES <107401263+GoncalvesDDaniel@users.noreply.github.com>
2024-06-25 13:37:10 +0200

    Home page init

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,34 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, id, price, tagline, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Photographers-Id-Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        const link = document.createElement("a");
+        link.setAttribute("href", "./photographer.html");
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        const h2 = document.createElement("h2");
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const pLocalisation = document.createElement("p");
+        pLocalisation.className = "photographe-location";
+        pLocalisation.textContent = `${city}, ${country}`;
+        const pTagline = document.createElement("p");
+        pTagline.textContent = tagline;
+        pTagline.className = "photographe-tagline";
+        const pPrice = document.createElement("p");
+        pPrice.className = "photographe-price";
+        pPrice.textContent = `${price}€/jour`;
+
+        link.appendChild(img);
+        link.appendChild(h2);
+
+        article.appendChild(link);
+        article.appendChild(pLocalisation);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
